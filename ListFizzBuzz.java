4ce9770e61c4c323cefd75c4d696061b025cb8fd
Yue Teng <cindylebron777@gmail.com>
2020-05-22 00:13:41 -0400

    20200521

---
+++ ListFizzBuzz.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> out = new ArrayList<String>(n);
+        for (int i = 1; i <= n; i++){
+            if (i%3 == 0 && i%5 == 0) {
+                out.add("FizzBuzz");
+            } else if (i%3 == 0) {
+                out.add("Fizz");
+            } else if (i%5 == 0){
+                out.add("Buzz");
+            } else {
+                out.add(Integer.toString(i));
+            }
+        }
+        return out;
+    }
+}
